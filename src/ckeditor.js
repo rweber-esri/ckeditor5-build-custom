4f8b307f29c0f7ddf7efa0172e24bb496b5b0736
Randy Weber <rweber@esri.com>
2022-06-15 12:08:45 -0400

    Add expanded plugins and toolbar configuration

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,24 +3,48 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
-const builtinPlugins = [
+export const PLUGINS = {
+	Bold,
 	Essentials,
+	Heading,
+	Italic,
+	Link,
 	Paragraph,
+	PasteFromOffice,
 	TextTransformation
-];
+};
 
-const defaultConfig = {
+export const defaultPlugins = Object.values(PLUGINS);
+
+export const defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'|',
+			'undo',
+			'redo'
+		]
+	},
 	language: 'en'
 };
 
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = builtinPlugins;
+ClassicEditor.builtinPlugins = defaultPlugins;
 
 // Editor configuration.
 ClassicEditor.defaultConfig = defaultConfig;
